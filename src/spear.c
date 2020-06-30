9a2f45d1505c0518a28dc921d5d5153d7cd853f5
Tomohiro Yamada <y.tomo.0524@gmail.com>
2020-06-30 20:35:49 +0900

    revised

--- src/spear.c
+++ src/spear.c
@@ -4,6 +4,14 @@ int n;
 int k;
 int A[100000];
 
+int p(int m){
+  int sum = 0;
+  for(int i = 0; i < n; i++){
+  int x = A[i] / m;
+  sum += x;
+  }
+  return sum >= k;
+}
 
 int main(){
   int i, lb, ub;
@@ -11,7 +19,21 @@ int main(){
   for(i = 0; i < n; i++){
     scanf("%d", &A[i]);
   }
-
-
+  lb = 0;
+  ub = A[0];
+  for(int j = 0; j < n; j++){
+    if (A[j] > ub){ 
+      ub = A[j];
+    }
+  }
+  while(ub - lb > 1){
+    int mid = (lb + ub) / 2;
+    if(p(mid)) {
+      lb = mid;
+    }else{
+      ub = mid;
+    }
+  }
+  printf("%d\n", lb);
   return 0;
 }
