a6698d666514073b4bdf7d2bcb0a41bd8adef6eb
Tomohiro Yamada <y.tomo.0524@gmail.com>
2020-06-30 19:33:38 +0900

    revised

--- src/array.c
+++ src/array.c
@@ -4,15 +4,19 @@ int n;
 int k;
 int A[100000];
 
-
 int main(){
   int i, lb, ub;
   scanf("%d%d", &n, &k);
+  lb = -1;
+  ub = n;
   for(i = 0; i < n; i++){
     scanf("%d", &A[i]);
   }
-
-
-
+  while(ub - lb > 1 ) {
+    int mid = (lb + ub) / 2;
+    if(A[mid] >= k) ub = mid;
+    else lb = mid;
+  }
+  printf("%d\n" , ub);
   return 0;
 }
