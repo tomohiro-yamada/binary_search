df76f4dc5adb7424833fb2588ad59662c1a54fde
Tomohiro Yamada <y.tomo.0524@gmail.com>
2020-07-04 12:10:33 +0900

    revised

--- src/works.c
+++ src/works.c
@@ -4,6 +4,17 @@ int n;
 int k;
 int A[100000];
 
+int p(int m){
+  int notLergerThanM = n;
+  for(int j = 0; j < n-1; j++){
+    if(A[j] + A[j+1] <= m){
+      A[j+1] = A[j+1] + A[j];
+      notLergerThanM --;
+    }
+  }
+  return notLergerThanM <= k; 
+}
+    
 
 int main(){
   int i, lb, ub;
@@ -11,7 +22,35 @@ int main(){
   for(i = 0; i < n; i++){
     scanf("%d", &A[i]);
   }
-
-
+  int max = A[0];
+  for(int q = 0; q < n; q++){
+    if(max < A[q]){
+      max = A[q];
+    }
+  }
+  int sum = 0;
+  for(int l = 0; l < n; l++){
+    sum += A[l];
+  }
+  if(max > sum / k){
+    lb = max;
+  }else{
+    lb = sum / k;
+  }
+  ub = sum;
+  while(ub - lb > 1){
+    int mid = (ub +  lb) / 2;
+    if(p(mid)){	
+      ub = mid;
+    }else{
+      lb = mid;
+    }
+  }
+  if(p(lb)){
+    printf("%d\n", lb);
+  }else{
+    printf("%d\n", ub);
+  }
   return 0;
 }
+  
